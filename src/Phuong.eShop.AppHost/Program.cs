0bfb296c6fea2631257fa158b3f209396dae1fb1
Phuong Vo <phuongvohoai@outlook.com>
2024-08-29 00:02:46 +0700

    Add blazor project

--- src/Phuong.eShop.AppHost/Program.cs
+++ src/Phuong.eShop.AppHost/Program.cs
@@ -1,16 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres").WithPgAdmin();
+var postgres = builder
+    .AddPostgres("postgres")
+    .WithDataVolume()
+    .WithPgAdmin();
 
 builder.AddProject<Projects.Phuong_eShop_IdentityService>("IdentityService")
     .WithReference(postgres.AddDatabase("identityDb"));
 
-var catalogService = builder.AddProject<Projects.Phuong_eShop_CatalogService>("CatalogService")
+builder.AddProject<Projects.Phuong_eShop_CatalogService>("CatalogService")
     .WithReference(postgres.AddDatabase("catalogDb"));
 
-
-/*builder.AddNpmApp("eShopWebApp", "../Phuong.eShop.WebApp", "dev")
-    .WithEnvironment("CATALOG_API_URL", catalogService.GetEndpoint("http"))
-    .WithHttpEndpoint(env: "PORT");*/
+builder.AddProject<Projects.Phuong_eShop_BlazorApp>("BlazorApp");
 
 builder.Build().Run();
