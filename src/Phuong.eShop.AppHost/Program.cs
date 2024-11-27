b56d65ed75dc0d0889e22d1ebbdebc905ab4890b
Vo Hoai Phuong <phuongvohoai@outlook.com>
2024-11-27 11:13:02 +0700

    update login on admin page

--- src/Phuong.eShop.AppHost/Program.cs
+++ src/Phuong.eShop.AppHost/Program.cs
@@ -1,13 +1,16 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres").WithDataVolume("postgres-data").WithPgAdmin();
+var username = builder.AddParameter("username");
+var password = builder.AddParameter("password");
+
+var postgres = builder.AddPostgres("postgres", username, password).WithDataVolume().WithPgAdmin();
 var identityDb = postgres.AddDatabase("identityDb");
 var catalogDb = postgres.AddDatabase("catalogDb");
 
-var keycloak = builder.AddKeycloak("keycloak", 8080);
+var keycloak = builder.AddKeycloak("keycloak", 8080, username, password).WithDataVolume();
 
 builder.AddProject<Projects.Phuong_eShop_IdentityService>("IdentityService").WithReference(identityDb);
 builder.AddProject<Projects.Phuong_eShop_CatalogService>("CatalogService").WithReference(catalogDb);
-builder.AddProject<Projects.Phuong_eShop_BlazorApp>("BlazorApp");
+builder.AddProject<Projects.Phuong_eShop_BlazorApp>("BlazorApp").WithReference(keycloak);
 
 builder.Build().Run();
