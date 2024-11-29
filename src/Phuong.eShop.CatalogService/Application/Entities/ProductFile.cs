103402236ebc6d399c5b9aa875d84c2bcdcf2992
hanam197 <hanam15c4a@gmail.com>
2024-11-29 20:53:38 +0700

    feature: upload and view file

---
+++ src/Phuong.eShop.CatalogService/Application/Entities/ProductFile.cs
@@ -0,0 +1,10 @@
+﻿namespace Phuong.eShop.CatalogService.Application.Entities
+{
+    public class ProductFile
+    {
+        public int Id { get; set; }
+        public string? FileName { get; set; }
+        public required byte[] Content { get; set; }
+
+    }
+}
---
+++ src/Phuong.eShop.CatalogService/Controllers/FileUploadController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Phuong.eShop.CatalogService.Controllers;
+
+[Route("api/files")]
+public class FileUploadController(ICatalogDbContext context) : BaseApiController
+{
+    [HttpPost("upload")]
+    public async Task<IActionResult> OnPostUploadAsync(IFormFile formFile, CancellationToken cancellationToken)
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            await formFile.CopyToAsync(memoryStream, cancellationToken);
+            if (memoryStream.Length < 2097152)
+            {
+                var file = new ProductFile()
+                {
+                    FileName = formFile.FileName,
+                    Content = memoryStream.ToArray()
+                };
+                context.ProductFiles.Add(file);
+                await context.SaveChangesAsync(cancellationToken);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+        return Ok();
+    }
+
+    [HttpGet("view/{Id:long}")]
+    public async Task<IActionResult> GetImageStream(long Id, CancellationToken cancellationToken)
+    {
+        var file = await context.ProductFiles.FirstOrDefaultAsync(e => e.Id == Id, cancellationToken);
+        if (file == null)
+        {
+            return NotFound();
+        }
+        return File(file.Content, "image/jpeg");
+    }
+
+}
+
