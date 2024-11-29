c10ca984edd950f958cadb39887ddb7d3a6c9157
hanam197 <162672592+hanam197@users.noreply.github.com>
2024-11-29 22:39:07 +0700

    feature: add catalog product crud API (#14)
    * feature: add catalog product crud api
    
    * chore: add newline between command & handler for Catalog Product

---
+++ src/Phuong.eShop.CatalogService/Application/CatalogProducts/Commands/CreateCatalogProductCommand.cs
@@ -0,0 +1,28 @@
+﻿using Phuong.eShop.CatalogService.Application.CatalogProducts.Models;
+
+namespace Phuong.eShop.CatalogService.Application.CatalogProducts.Commands
+{
+    public record CreateCatalogProductCommand(CatalogProductDto CatalogProduct) : IRequest<ApiResponse<CatalogProductDto>>;
+
+    public class CreateCatalogProductCommandHandler(ICatalogDbContext context, IUserService userService) : IRequestHandler<CreateCatalogProductCommand, ApiResponse<CatalogProductDto>>
+    {
+        public async Task<ApiResponse<CatalogProductDto>> Handle(CreateCatalogProductCommand command, CancellationToken cancellationToken)
+        {
+            var catalogProduct = new CatalogItem
+            {
+                Name = command.CatalogProduct.Name,
+                Description = command.CatalogProduct.Description,
+                Price = command.CatalogProduct.Price,
+                PictureUri = "",
+                CatalogTypeId = command.CatalogProduct.CatalogTypeId,
+                CatalogBrandId = command.CatalogProduct.CatalogBrandId,
+                AvailableStock = command.CatalogProduct.AvailableStock,
+                CreatedAt = DateTime.UtcNow,
+                CreatedBy = userService.Name,
+            };
+            context.CatalogItems.Add(catalogProduct);
+            await context.SaveChangesAsync(cancellationToken);
+            return catalogProduct.Adapt<CatalogProductDto>();
+        }
+    }
+}
